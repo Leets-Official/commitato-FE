646c1f31320549f6464c878b394b0e82562cde02
dahhyeon <dahhyeon22@gmail.com>
2024-07-21 18:34:13 +0900

    feat:검색바 ui 구현

--- src/pages/RankingPage/RankingPage.jsx
+++ src/pages/RankingPage/RankingPage.jsx
@@ -8,8 +8,9 @@ import {
 } from '../MyPage/MyPage';
 import line from '../../assets/line.png';
 import styled from 'styled-components';
-
+import Search from '../../assets/Search.png';
 const RankingPage = () => {
+  const onChange = () => {};
   return (
     <>
       <Header />
@@ -17,6 +18,10 @@ const RankingPage = () => {
         <RankingContainer>
           <StyledTitle>Ranking</StyledTitle>
           <StyledImg src={line} />
+          <InputDiv>
+            <Input type="text" onChange={onChange} placeholder="Search Here" />
+            <InputImg src={Search} alt="Search" />
+          </InputDiv>
         </RankingContainer>
       </StyledBlack>
       <Footer />
@@ -32,4 +37,30 @@ const RankingContainer = styled(StyledWhite)`
   height: 900px;
   background: ${({ theme }) => theme.COLORS.white};
   margin: auto auto;
+`;
+
+const InputDiv = styled.div`
+  position: relative;
+  width: 1100px;
+`;
+
+const Input = styled.input`
+  &::placeholder {
+    font-family: ${({ theme }) => theme.FONT_FAMILY.main};
+    font-size: ${({ theme }) => theme.FONT_SIZE.medium};
+    color: ${({ theme }) => theme.COLORS.gray[100]};
+  }
+  border: 4px solid ${({ theme }) => theme.COLORS.yellow[100]};
+  border-radius: 49px;
+  width: 100%;
+  padding: 20px;
+  margin: 30px 70px;
+`;
+
+const InputImg = styled.img`
+  position: absolute;
+  width: 40px;
+  top: 40px;
+  left: 1150px;
+  margin: 0;
 `;
