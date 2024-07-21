b47e072c7c10268529c17a1bb6bb92e97f5b8f93
dahhyeon <dahhyeon22@gmail.com>
2024-07-21 20:42:59 +0900

    feat:mock data로 랭킹아이템 컴포넌트 렌더링

---
+++ src/pages/RankingPage/components/RankingItem.jsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+const RankingItem = ({ id, rank, user, tier, commitDay, exp }) => {
+  return (
+    <ItemDiv>
+      <span>{rank}</span>
+      <span>{user}</span>
+      <span>{tier}</span>
+      <span>{commitDay}</span>
+      <span>{exp}</span>
+    </ItemDiv>
+  );
+};
+
+export default RankingItem;
+
+const ItemDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  gap: 100px;
+
+  font-family: ${({ theme }) => theme.FONT_FAMILY.pretendard[300]};
+  font-size: ${({ theme }) => theme.FONT_SIZE.medium};
+`;
--- src/pages/RankingPage/components/RankingList.jsx
+++ src/pages/RankingPage/components/RankingList.jsx
@@ -1,14 +1,66 @@
 import styled from 'styled-components';
+import RankingItem from './RankingItem';
+
+const rankingItems = [
+  {
+    id: 1,
+    rank: 1,
+    user: 'githubID1',
+    tier: 'CEO 감자',
+    commitDay: '7일',
+    exp: 120,
+  },
+  {
+    id: 2,
+    rank: 2,
+    user: 'githubID2',
+    tier: 'CEO 감자',
+    commitDay: '5일',
+    exp: 110,
+  },
+  {
+    id: 3,
+    rank: 3,
+    user: 'githubID3',
+    tier: 'CEO 감자',
+    commitDay: '4일',
+    exp: 100,
+  },
+  {
+    id: 4,
+    rank: 4,
+    user: 'githubID4',
+    tier: '개발자 감자',
+    commitDay: '4일',
+    exp: 90,
+  },
+  {
+    id: 5,
+    rank: 5,
+    user: 'githubID5',
+    tier: '개발자 감자',
+    commitDay: '4일',
+    exp: 80,
+  },
+];
 
 const RankingList = () => {
   return (
-    <ListDiv>
-      <p>Rank</p>
-      <p>User</p>
-      <p>Tier</p>
-      <p>연속 커밋 횟수</p>
-      <p>경험치</p>
-    </ListDiv>
+    <>
+      <ListDiv>
+        <p>Rank</p>
+        <p>User</p>
+        <p>Tier</p>
+        <p>연속 커밋 횟수</p>
+        <p>경험치</p>
+      </ListDiv>
+      <div>
+        {rankingItems.map(item => {
+          return <RankingItem key={item.id} {...item} />;
+        })}
+      </div>
+      <RankingItem />
+    </>
   );
 };
 
