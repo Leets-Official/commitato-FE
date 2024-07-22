2ec60f46de315a3ed0489b3e17cb08d9dd03eb6c
dahhyeon <dahhyeon22@gmail.com>
2024-07-22 20:36:37 +0900

    chore:rankingItems mockdata 갯수 추가

--- src/pages/RankingPage/components/RankingItem.jsx
+++ src/pages/RankingPage/components/RankingItem.jsx
@@ -1,10 +1,15 @@
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 const RankingItem = ({ id, rank, user, tier, commitDay, exp }) => {
+  const nav = useNavigate();
+  const onClickToMyPage = () => {
+    nav('/my');
+  };
   return (
     <ItemDiv>
       <span>{rank}</span>
-      <span>{user}</span>
+      <UserItem onClick={onClickToMyPage}>{user}</UserItem>
       <span>{tier}</span>
       <span>{commitDay}</span>
       <span>{exp}</span>
@@ -21,4 +26,11 @@ const ItemDiv = styled.div`
   padding: 15px 80px;
   font-family: ${({ theme }) => theme.FONT_FAMILY.pretendard[300]};
   font-size: ${({ theme }) => theme.FONT_SIZE.small};
+`;
+
+const UserItem = styled.span`
+  cursor: pointer;
+  &:hover {
+    color: ${({ theme }) => theme.COLORS.brown[100]};
+  }
 `;
