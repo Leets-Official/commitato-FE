b79aa76bebdd49e80ef33776cdb6ee0473915e45
dahhyeon <dahhyeon22@gmail.com>
2024-07-28 23:20:49 +0900

    feat:webkit-scroller 스타일 설정

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -10,6 +10,27 @@ const GlobalStyles = createGlobalStyle`
         overflow-y: auto;
         height: 100%;
     }
+
+    ::-webkit-scrollbar {
+        width: 15px;
+        height: 30px;
+    }
+
+    ::-webkit-scrollbar-track {
+        background-color: ${({ theme }) => theme.COLORS.black};
+        border-radius: 6px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background-color: ${({ theme }) => theme.COLORS.yellow[100]};
+        border-radius: 6px;
+        border: 3px solid #000;
+    }
+
+    ::-webkit-scrollbar-thumb:hover {
+        background-color: ${({ theme }) => theme.COLORS.yellow[200]};
+    }
+
     @font-face {
         font-family:"Staatliches-Regular" ;
         src: url("fonts/Staatliches-Regular.woff") format("woff");
