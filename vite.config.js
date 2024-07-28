0149e824dda3dbc4628a290eb47107c782111a5c
Jiwon Kim <jiwonkim0531@gachon.ac.kr>
2024-07-28 17:34:12 +0900

    엑세스 토큰 변경 후 연결

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react(), svgr()],
+  server: {
+    proxy: {
+      '/login/github': {
+        target:
+          'http://ec2-43-201-143-81.ap-northeast-2.compute.amazonaws.com:8080', // 백엔드 서버 주소
+        changeOrigin: true,
+        secure: false,
+        rewrite: path => path.replace(/^\/login\/github/, ''),
+      },
+    },
+  },
 });
