22fb4f45e97eadf879cbb9a020342eaafc77cf91
dahhyeon <dahhyeon22@gmail.com>
2024-07-31 01:09:07 +0900

    desing:검색바 크기,위치 조정

--- src/pages/RankingPage/components/RankingList.jsx
+++ src/pages/RankingPage/components/RankingList.jsx
@@ -60,25 +60,25 @@ const InputDiv = styled.div`
 const Input = styled.input`
   &::placeholder {
     font-family: ${({ theme }) => theme.FONT_FAMILY.main};
-    font-size: ${({ theme }) => theme.FONT_SIZE.medium};
+    font-size: 24px;
     color: ${({ theme }) => theme.COLORS.gray[100]};
   }
 
   font-family: ${({ theme }) => theme.FONT_FAMILY.pretendard[300]};
   font-size: ${({ theme }) => theme.FONT_SIZE.medium};
 
-  border: 4px solid ${({ theme }) => theme.COLORS.yellow[100]};
+  border: 3px solid ${({ theme }) => theme.COLORS.yellow[100]};
   border-radius: 49px;
   width: 100%;
-  padding: 20px 15px 15px 30px;
-  margin: 30px 70px;
+  padding: 9px 7px 7px 22px;
+  margin: 20px 55px;
 `;
 
 const InputImg = styled.img`
   position: absolute;
-  width: 40px;
-  top: 48px;
-  left: 950px;
+  width: 33px;
+  top: 36px;
+  left: 930px;
 `;
 
 const ListContainer = styled.div`
