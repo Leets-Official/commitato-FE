441662503ebe5e60bbe7f5b990aaab77584f6941
Jiwon Kim <jiwonkim0531@gachon.ac.kr>
2024-08-09 16:23:43 +0900

    .

--- src/apis/auth/Login.jsx
+++ src/apis/auth/Login.jsx
@@ -35,87 +35,9 @@
 // export default githubLogin;
 // export default loginCallback;
 
-import React, { useEffect, useState } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  useLocation,
-  useNavigate,
-} from 'react-router-dom';
+import React from 'react';
 import { GithubButton } from '../../pages/MainPage/MainPage';
-import axios from 'axios';
 
-const Home = () => <GithubButton />;
-
-const Callback = () => {
-  const { search } = useLocation();
-  const navigate = useNavigate();
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [accessToken, setAccessToken] = useState(null);
-  const [refreshToken, setRefreshToken] = useState(null);
-  const [hasRedirected, setHasRedirected] = useState(false); // 추가된 상태
-
-  useEffect(() => {
-    const fetchToken = async () => {
-      const code = new URLSearchParams(search).get('code');
-      if (code) {
-        try {
-          const response = await axios.get('api/login/callback', {
-            params: { code },
-          });
-          console.log('Response:', response.data);
-          const { accessToken, refreshToken } = response.data.result;
-          localStorage.setItem('ACCESS_TOKEN_KEY', accessToken);
-          localStorage.setItem('REFRESH_TOKEN_KEY', refreshToken);
-          console.log('Tokens:', accessToken, refreshToken);
-
-          // Set the tokens to state for displaying
-          setAccessToken(accessToken);
-          setRefreshToken(refreshToken);
-          setHasRedirected(true); // 리다이렉트 플래그 설정
-        } catch (err) {
-          console.error('Error fetching tokens:', err);
-          setError('Failed to fetch tokens');
-        }
-      } else {
-        setError('Authorization code not found');
-      }
-      setLoading(false);
-    };
-
-    fetchToken();
-  }, [search]);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>{error}</div>;
-  }
-
-  return (
-    <div>
-      <h1>Authenticated successfully!</h1>
-      <p>
-        <strong>Access Token:</strong> {accessToken}
-      </p>
-      <p>
-        <strong>Refresh Token:</strong> {refreshToken}
-      </p>
-    </div>
-  );
-};
-
-const Login = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Callback />} />
-    </Routes>
-  </Router>
-);
+const Login = () => <GithubButton />;
 
 export default Login;
